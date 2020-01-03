f660fb79fb5efa84fcfa498f3c89083233d8755e
Javier Olayon <olayon.javier@gmail.com>
2020-01-03 14:38:14 -0500

    partial work on chapter 4

---
+++ chp4.py
@@ -0,0 +1,27 @@
+#Comma Code
+# def commaCode(value):
+#     for item in value:
+#         if item == value[len(value) - 1 ]:
+#             print('and ' + item)
+#         else:
+#             print(item + ', ', end ='')
+
+# test = ['apples', 'bananas', 'tofu', 'cats']
+# commaCode(test)
+#------------------------------------------------------------------------------------
+#Coin Flip Streaks
+import random
+numberOfStreaks = 0
+for experimentNumber in range(10000):
+    # Code that creates a list of 100 'heads' or 'tails' values.
+    values = []
+    for i in range (1, 100):
+        values.append(random.randint(0, 1))
+    # Code that checks if there is a streak of 6 heads or tails in a row.
+    for i in range
+    print (values)
+print('Chance of streak: %s%%' % (numberOfStreaks / 100))
+
+
+
+#------------------------------------------------------------------------------------ 
